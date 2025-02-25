7dad47703b50bd69ffd9bbcd4259bd657d54f9e8
HighTLCharoda <hightlcharoda@gmail.com>
2025-02-25 23:39:32 +0300

    blank window

--- js/main.js
+++ js/main.js
@@ -1,62 +1,32 @@
-class LeafletTileLayer extends CanvasLayer {
-  static get layerOptions() {
-    return foundry.utils.mergeObject(super.layerOptions, {
-      zIndex: 100,
-      name: "LeafletMap"
-    });
+class PopoutWindow extends FormApplication {
+  static get defaultOptions() {
+      return foundry.utils.mergeObject(super.defaultOptions, {
+          id: "interactive-map-popout",
+          template: "modules/interactive-map-popout/templates/map-popout.html",
+          width: 800,
+          height: 600,
+          resizable: true,
+          closeOnSubmit: false,
+          popOut: true
+      });
   }
 
-  async draw() {
-    // Удаляем старую карту, если существует
-    if (this.map) this.map.remove();
-
-    // Создаем контейнер для Leaflet
-    this.leafletDiv = document.createElement("div");
-    this.leafletDiv.id = "leaflet-map";
-    Object.assign(this.leafletDiv.style, {
-      width: "100%",
-      height: "100%",
-      position: "absolute",
-      top: "0",
-      left: "0"
-    });
-
-    // Добавляем контейнер в слой
-    this.canvas.background.addChild(this.leafletDiv);
-
-    // Инициализация Leaflet
-    this.map = L.map(this.leafletDiv, {
-      crs: L.CRS.Simple,
-      minZoom: 3,
-      maxZoom: 8,
-      attributionControl: false,
-      interactive: true
-    });
-
-    // Загрузка тайлов
-    this.tileLayer = L.tileLayer('modules/leaflet-tiles/tiles/{z}/{x}/{y}.webp', {
-      tileSize: 512,
-      noWrap: true
-    }).addTo(this.map);
-
-    // Центрирование карты
-    const bounds = [[0,0], [this.canvas.dimensions.height, this.canvas.dimensions.width]];
-    this.map.fitBounds(bounds);
-
-    // Включение зума колесом
-    this.map.scrollWheelZoom.enable();
+  activateListeners(html) {
+      super.activateListeners(html);
+      html.find(".close").click(() => this.close());
   }
 }
 
-// Регистрация слоя
-Hooks.once('init', () => {
-  CONFIG.Canvas.layers.leafletTiles = {
-    group: "primary",
-    layerClass: LeafletTileLayer
-  };
-});
-
-// Активация слоя при создании сцены
-Hooks.on("canvasInit", (canvas) => {
-  canvas.leafletTiles = new LeafletTileLayer();
+Hooks.once("init", () => {
+  game.keybindings.register("interactive-map-popout", "toggleWindow", {
+      name: "Toggle Map Window",
+      editable: [{ key: "KeyM" }],
+      onDown: () => {
+          const existing = Object.values(ui.windows).find(w => w.id === "interactive-map-popout");
+          if (existing) existing.close();
+          else new PopoutWindow().render(true);
+          return true;
+      },
+      restricted: true
+  });
 });
